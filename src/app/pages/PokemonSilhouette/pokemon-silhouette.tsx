f8e7b201ff2e1dabb92c2c58c102d3e0a59c046d
Gemdelle <milu200911@hotmail.com>
2023-05-28 17:52:56 -0300

    Board style.

--- src/app/pages/PokemonSilhouette/pokemon-silhouette.tsx
+++ src/app/pages/PokemonSilhouette/pokemon-silhouette.tsx
@@ -8,36 +8,46 @@ import data from '../../../data/data.json';
 import { useState } from 'react';
 
 export default function PokemonSilhouette() {
-  const [currentPlayer1Round, setCurrentPlayer1Round] = useState(0)
-  const [currentPlayer2Round, setCurrentPlayer2Round] = useState(0)
+  const [currentPlayer1Round, setCurrentPlayer1Round] = useState(0);
+  const [currentPlayer2Round, setCurrentPlayer2Round] = useState(0);
 
   const navigate = useNavigate();
 
   const homePage = () => navigate('/');
 
-  const player1 = Object.keys(data.pokemons).map((key) => {
+  const player1 = Object.keys(data.pokemons).map(key => {
     return data.pokemons[key][0];
   });
 
-  const player2 = Object.keys(data.pokemons).map((key) => {
+  const player2 = Object.keys(data.pokemons).map(key => {
     return data.pokemons[key][1];
-  })
+  });
 
   return (
-    <>
-      <div className='intro'>
+    <div className="container">
+      {/* <div className="intro">
         <div>
-          <span>Player 1:</span><input placeholder='Insert Name of Player 1'></input>
+          <span>Player 1:</span>
+          <input placeholder="Insert Name of Player 1"></input>
         </div>
         <div>
-          <span>Player 2:</span><input placeholder='Insert Name of Player 2'></input>
+          <span>Player 2:</span>
+          <input placeholder="Insert Name of Player 2"></input>
         </div>
-      </div>
+      </div> */}
       <div className="board-container">
-        <Board pokemonUrl={"/images/"+player1[currentPlayer1Round]+".png"} roundNumber={currentPlayer1Round} />
-        <Board pokemonUrl={"/images/"+player2[currentPlayer2Round]+".png"} roundNumber={currentPlayer2Round} />
-        <input type='text'></input>
+        <Board
+          pokemonUrl={'/images/' + player1[currentPlayer1Round] + '.png'}
+          roundNumber={currentPlayer1Round}
+        />
+        <div className="bar"></div>
+        <div className="timer"></div>
+        <Board
+          pokemonUrl={'/images/' + player2[currentPlayer2Round] + '.png'}
+          roundNumber={currentPlayer2Round}
+        />
+        <input type="text"></input>
       </div>
-    </>
+    </div>
   );
 }
